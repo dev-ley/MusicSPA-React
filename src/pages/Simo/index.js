a866d460e13f76b5a7623e848cf8fe2136d67043
Wesley Rodrigues <122097397+dev-ley@users.noreply.github.com>
2023-09-06 00:15:23 -0300

    background

--- src/pages/Simo/index.js
+++ src/pages/Simo/index.js
@@ -1,14 +1,14 @@
+import './Simo.css'
+
 function Simo () {
     return(
       <>
-      <div>
+      <div className='fundo'>
             <h1>Feel the music</h1>
             <p>Stream over 10 million songs with one click</p>
             <button>Join Now</button>
        </div>
-        <div>
-            <img src="" ></img>
-        </div>
+      
      </>
     )
 }
